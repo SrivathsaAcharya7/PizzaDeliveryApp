77a0cfda55528568918e4ae6bb827837660f9d6d
SrivathsaAcharya7 <srivathsa.acharya@triconinfotech.com>
2024-08-01 10:42:58 +0530

    feat: order by id api

--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,76 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from fastapi_jwt_auth import AuthJWT
 
-order_router=APIRouter(
+from database import Session, engine
+from models import Order
+from models import User
+from schemas import OrderModel
+
+order_router = APIRouter(
     prefix="/orders"
 )
+session = Session(bind=engine)
+
 
 @order_router.get("/")
-async def hello():
+async def hello(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
     return {"This is orders page"}
 
+
+@order_router.post("/orders")
+async def place_an_order(order: OrderModel, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
+
+    current_user = Authorize.get_jwt_subject()
+    user = session.query(User).filter(User.username == current_user).filter()
+
+    new_order = Order(
+        pizza_size=order.pizza_size,
+        quantity=order.quantity
+    )
+    new_order.user = user
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "pizza_size": new_order.pizza_size,
+        "quantity": new_order.quantity,
+        "id": new_order.id,
+        "order_status": new_order.order_status
+    }
+
+    return jsonable_encoder(response)
+
+
+@order_router.get("/orders")
+async def get_all_orders(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
+
+    current_user = Authorize.get_jwt_subject()
+    user = session.query(User).filter(User.username == current_user).first()
+
+    if user.is_staff:
+        orders = session.Query(Order).all()
+
+        return jsonable_encoder(orders)
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not a superuser")
+
+
+@order_router.get("/orders/{id}")
+async def get_order_by_id(id: int, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid user")
+    
---
+++ schemas.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class SignupModel(BaseModel):
+    id: Optional[int]
+    username: str
+    email: str
+    password: str
+    is_staff: Optional[bool] = Field(default=False)
+    is_active: Optional[bool] = Field(default=True)
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                "username": "johndoe",
+                "email": "johndoe@gmail.com",
+                "password": "password",
+                "is_staff": False,
+                "is_active": True,
+            }
+        }
+
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = "be6fff5ddd03309638c98370e53b67fb2b4c375d6ed53d0b3030dfdf0ddb5ed"
+
+
+class LoginModel(BaseModel):
+    username: str
+    password: str
+
+
+class OrderModel(BaseModel):
+    id = Optional[int]
+    quantity = int
+    order_status = Optional[str] = "PENDING"
+    pizza_size = Optional[str] = "SMALL"
+    user_id = Optional[int]
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "quantity": 2,
+                "pizza_size": "LARGE"
+            }
+        }
